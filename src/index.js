7cd486798e99144f4fcdcbb1f19fd2d966fd9f4b
Kirill Panferov <kirillpanferov.daw@ciudadescolarfp.es>
2021-04-12 09:39:06 +0200

    Docs agregado

--- src/index.js
+++ src/index.js
@@ -9,4 +9,22 @@ ReactDOM.render(<GifExpertApp value={100}/> ,divRoot);
 
 /*
 npm run build
+npm install --global http-server
+cd a build
+http-server -o
+git config --global user.name "Mona Lisa"
+git config --global user.email "kirillpanferov.daw@ciudadescolarfp.es"
+git init
+git add .
+git commit -m "First Commit"
+rename build to docs
+
+
+
+
+
+
+
+
+
 */
